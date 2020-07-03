8b7fadf5fb5c2e36c92c6ebde938fd121f2a875d
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-03 12:01:03 +0200

    add foreground

--- game.js
+++ game.js
@@ -25,12 +25,29 @@ const bg = {
   }
 }
 
+// foreground
+const fg = {
+  sX: 276,
+  sY: 0,
+  w: 224,
+  h: 112,
+  x: 0,
+  y: cvs.height - 112,
+
+  draw: function() {
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+  }
+}
+
 // draw function
 function draw() {
   ctx.fillStyle = "#70C5CE";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
 
   bg.draw();
+  fg.draw();
 }
 
 // update function
