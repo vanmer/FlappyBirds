a6337c75b827dced1f6801cbd8d580e2be9065aa
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-03 02:43:29 +0200

    draw background

--- game.js
+++ game.js
@@ -9,10 +9,28 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+// background
+const bg = {
+  sX: 0,
+  sY: 0,
+  w: 275,
+  h: 226,
+  x: 0,
+  y: cvs.height - 226,
+
+  draw: function() {
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+
+    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+  }
+}
+
 // draw function
 function draw() {
   ctx.fillStyle = "#70C5CE";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+  bg.draw();
 }
 
 // update function
