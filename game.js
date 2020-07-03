945f09f89dd2d190558f03283c0fd6ae6c507f75
vanmer <60948275+vanmer@users.noreply.github.com>
2020-07-03 14:58:12 +0200

    add the pipe

--- game.js
+++ game.js
@@ -173,12 +173,69 @@ const gameOver = {
   }
 }
 
+// pipes
+const pipes = {
+  position: [],
+
+  top: {
+    sX: 553,
+    sY: 0
+  },
+
+  bottom: {
+    sX: 502,
+    sY: 0
+  },
+
+  w: 53,
+  h: 400,
+  gap: 85,
+  maxYPos: -150,
+  dx: 2,
+
+  draw: function() {
+    for (let i = 0; i < this.position.length; i++) {
+      let p = this.position[i];
+      let topYPos = p.y;
+      let bottomYPos = p.y + this.h + this.gap;
+
+      // top pipe
+      ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+
+      // bottom pipe
+      ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
+    }
+  },
+
+  update: function() {
+    if (state.current !== state.game) return;
+
+    if (frames % 100 == 0) {
+      this.position.push({
+        x: cvs.width,
+        y: this.maxYPos * (Math.random() + 1)
+      });
+    }
+    for (let i = 0; i < this.position.length; i++) {
+      let p = this.position[i];
+      p.x -= this.dx;
+      // if pipes go beyond canvas, we remove them from the array
+      if (p.x + this.w <= 0) {
+        this.position.shift();
+      }
+    }
+
+
+  }
+}
+
 // draw function
 function draw() {
   ctx.fillStyle = "#70C5CE";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
 
   bg.draw();
+  pipes.draw();
   fg.draw();
   bird.draw();
   getReady.draw();
@@ -188,7 +245,8 @@ function draw() {
 // update function
 function update() {
   bird.update();
-  fg.update()
+  fg.update();
+  pipes.update();
 }
 
 // loop/move function
